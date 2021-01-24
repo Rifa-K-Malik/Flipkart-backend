e4358fc3f6d196689cb34de1384497e8cbcf9826
Rifa-K-Malik <malikrif07@gmail.com>
2021-01-24 23:32:37 +0530

    2-Created: User

---
+++ src/models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    hash_password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'admin'
+    },
+    contactNumber: {
+        type: String
+    },
+    profilePicture: {
+        type: String
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('User', userSchema);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,47 @@
 const express = require('express');
+const env = require('dotenv');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// routes
+const userRoutes = require('./routes/user');
+
+
+env.config();
+
+// mongodb connection 
+
+mongoose.connect( `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@node-rest-shop.udknm.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority` ,
+    {
+    useUnifiedTopology:true,
+    useNewUrlParser : true,
+    useCreateIndex: true
+    }
+).then(res=>{console.log('connected')}).catch(err=>{console.log(err)});
+
+// mongoose
+//   .connect(
+//     `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.8pl1w.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//     }
+//   )
+//   .then(() => {
+//     console.log("Database connected");
+//   });
+
+mongoose.Promise = global.Promise;
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api', userRoutes);
+// app.use(bodyParser());
 
 app.listen(process.env.PORT, () => {
-    console.log('server is running on port ${process.env.PORT}');
+    console.log(`server is running on port ${process.env.PORT}`);
 }); 
