c9e305d27a08373bf1306d76245e80936426e605
Rifa-K-Malik <malikrif07@gmail.com>
2021-01-27 16:50:38 +0530

    Added Validator

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { signup, signin } = require('../../controller/admin/auth');
+const { validateSigninRequest, validateSignupRequest , isRequestValidated} = require('../../validators.js/auth');
 const router = express.Router();
 
-router.post('/admin/signin', signin);
+router.post('/admin/signin',validateSigninRequest , isRequestValidated, signin);
 
-router.post('/admin/signup', signup);
+router.post('/admin/signup',validateSignupRequest, isRequestValidated, signup);
 
 module.exports = router; 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { signup, signin } = require('../controller/auth');
+const { validateSigninRequest, validateSignupRequest, isRequestValidated } = require('../validators.js/auth');
 const router = express.Router();
 
-router.post('/signin', signin);
+router.post('/signin', validateSigninRequest, isRequestValidated, signin);
 
-router.post('/signup', signup);
+router.post('/signup',validateSignupRequest, isRequestValidated, signup);
 
 // router.post('/profile', requireSignin, (req, res) =>{
 //     res.status(200).json({ user: 'profile' })
