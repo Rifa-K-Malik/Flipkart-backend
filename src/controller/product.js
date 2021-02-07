17c2cef314f4fa4945272fa1c0b1e45f2d2211df
Rifa-K-Malik <malikrif07@gmail.com>
2021-02-07 10:42:01 +0530

    Uploading Images

---
+++ src/controller/product.js
@@ -0,0 +1,48 @@
+const Product = require('../models/product');
+const shortid = require('shortid');
+const slugify = require("slugify");
+
+exports.createProduct =  (req, res) => {
+    const { name, price, description, category, quantity, createdBy } = req.body;
+  let productPictures = [];
+
+  if (req.files.length > 0) {
+    productPictures = req.files.map((file) => {
+      return { img: file.location };
+    });
+  }
+
+  const product = new Product({
+    name: name,
+    slug: slugify(name),
+    price,
+    quantity,
+    description,
+    productPictures,
+    category,
+    createdBy: req.user._id,
+  });
+
+  product.save((error, product) => {
+    if (error) return res.status(400).json({ error });
+    if (product) {
+      res.status(201).json({ product, files: req.files });
+    }
+  });
+}; 
+
+
+
+
+
+
+
+
+
+
+// 34.28
+
+
+
+
+
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,14 +1,24 @@
 const express = require('express');
 //const { addCategory, getCategories } = require('../controller/category');
 const { requireSignin, adminMiddleware } = require('../common-middleware/index');
+const { createProduct } = require('../controller/product');
+const multer = require('multer');
 const router = express.Router();
+const shortid = require('shortid');
+const path = require('path');
 
-router.post('/product/create', requireSignin, adminMiddleware, (req, res) => {
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(path.dirname(__dirname), "uploads")      );
+    },
+    filename: function (req, file, cb) {
+      cb(null, shortid.generate() + "-" + file.originalname);
+    },
+  });
 
-    res.status(200).json({ message: 'Hello'})
-});
+  const upload = multer({ storage });
+
+router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
 //router.get('/product/getcategory', getCategories);
-
 module.exports = router;
-
-//0.12.06
+ 
