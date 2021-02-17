a647004f8c51f32fbadf376d0e12c91085e9dc35
Rifa-K-Malik <malikrif07@gmail.com>
2021-02-18 01:57:46 +0530

    2-cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,27 +1,9 @@
-const express = require("express");
-const {
-  addItemToCart,
-  addToCart,
-  getCartItems,
-  removeCartItems,
-} = require("../controller/cart");
-const { requireSignin, userMiddleware } = require("../common-middleware");
+const express = require('express');
+const { addItemToCart } = require('../controller/cart');
+const { requireSignin, userMiddleware } = require('../common-middleware/index');
 const router = express.Router();
 
-router.post(
-  "/user/cart/addtocart",
-  requireSignin,
-  userMiddleware,
-  addItemToCart
-);
-//router.post('/user/cart/addToCartByLogin', requireSignin, userMiddleware, addToCart);
-router.post("/user/getCartItems", requireSignin, userMiddleware, getCartItems);
-//new update
-router.post(
-  "/user/cart/removeItem",
-  requireSignin,
-  userMiddleware,
-  removeCartItems
-);
+router.post('/user/cart/addtocart', requireSignin, userMiddleware, addItemToCart);
 
 module.exports = router;
+
--- src/server.js
+++ src/server.js
@@ -47,6 +47,7 @@ app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', cartRoutes);
+
 // app.use(bodyParser());
 
 app.listen(process.env.PORT, () => {
