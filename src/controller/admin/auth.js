894f22a607194a3f53d13cab9fc32b4043a5af0b
Rifa-K-Malik <malikrif07@gmail.com>
2021-02-20 13:31:17 +0530

    admin sigout api

--- src/controller/admin/auth.js
+++ src/controller/admin/auth.js
@@ -47,6 +47,7 @@ exports.signin = (req, res) => {
             if(user.authenticate(req.body.password) && user.role === 'admin'){
                 const token = jwt.sign({_id: user._id, role: user.role}, process.env.JWT_SECRET, { expiresIn: '1h'});
                 const { _id, firstName, lastName, email, role, fullName } = user;
+                res.cookie('token', token, { expiresIn: '1h'});
                 res.status(200).json({
                     token,
                     user: {_id, firstName, lastName, email, role, fullName}
@@ -62,3 +63,9 @@ exports.signin = (req, res) => {
     });
 }
 
+exports.signout = (req, res) => { 
+    res.clearCookie("token");
+    res.status(200).json({
+      message: "Signout successfully...!",
+    });
+  };
--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { signup, signin } = require('../../controller/admin/auth');
+const { requireSignin } = require('../../common-middleware');
+const { signup, signin, signout } = require('../../controller/admin/auth');
 const { validateSigninRequest, validateSignupRequest , isRequestValidated} = require('../../validators.js/auth');
 const router = express.Router();
 
-router.post('/admin/signin',validateSigninRequest , isRequestValidated, signin);
 
+router.post('/admin/signin',validateSigninRequest , isRequestValidated, signin);
 router.post('/admin/signup',validateSignupRequest, isRequestValidated, signup);
+router.post('/admin/signout', signout);
+
 
 module.exports = router; 
